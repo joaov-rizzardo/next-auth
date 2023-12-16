bf31becfc8b203ba751ac2afe7af3c60f084cff1
joaov-rizzardo <jvitor2000@hotmail.com>
2023-12-16 12:03:18 -0300

    Access token

--- frontend/pages/auth-page-ssr.js
+++ frontend/pages/auth-page-ssr.js
@@ -1,7 +1,21 @@
-export default function AuthPageSSR(props) {
+import nookies from "nookies";
+import { tokenService } from "../src/services/auth/tokenService";
+
+function AuthPageSSR(props) {
   return (
     <div>
       <h1>Auth Page Server Side Render</h1>
+      <pre>{JSON.stringify(props, null, 2)}</pre>
     </div>
   );
+}
+
+export default AuthPageSSR;
+
+export async function getServerSideProps(ctx) {
+  return {
+    props: {
+      token: tokenService.get(ctx),
+    },
+  };
 }
